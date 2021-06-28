00a754258e41f406896a28fbc47c813d06e60ce4
Ahmed Masud <ahmed.masud@trustifier.com>
2021-06-28 13:20:27 -0400

    first commit

---
+++ _static/_version_info_GENERATED.py
@@ -0,0 +1,10 @@
+version_info = (
+{
+  "build_mtime": "1618252661",
+  "module_name": "sphinx_typo3_theme",
+  "version_scm": "4.4.4.dev1+g9adb58a",
+  "version_scm_build": "g9adb58a",
+  "version_scm_core": "4.4.4",
+  "version_scm_pre_release": "dev1"
+}
+)
